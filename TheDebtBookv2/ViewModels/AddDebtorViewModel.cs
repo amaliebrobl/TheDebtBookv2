8c0b0d75111320e15eb99cba93998263ccf89d4c
Amalie Bjørndal Robl <202108833@post.au.dk>
2024-03-07 11:48:24 +0100

    AddDebtorViewModel er tilføjet

---
+++ TheDebtBookv2/ViewModels/AddDebtorViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TheDebtBookv2.Data;
+using TheDebtBookv2.Models;
+
+namespace TheDebtBookv2.ViewModels
+{
+    public partial class AddDebtorViewModel : ObservableObject
+    {
+        private Database _database = new Database();
+        [ObservableProperty] private string? name;
+
+        [RelayCommand]
+        public void AddDebtor()
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                return;
+            }
+
+            var debtor = new Debtor()
+            {
+                Name = name
+            };
+
+            AddDebtorAsync(debtor);
+        }
+
+        private async void AddDebtorAsync(Debtor debtor)
+        {
+            await _database.AddDebtor(debtor);
+            Name = "";
+        }
+    }
+}
